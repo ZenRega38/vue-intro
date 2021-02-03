5d159027160fec7b6fcee4afa969f11b937e0ca2
ZenRega38 <regarizz@gmail.com>
2021-02-03 10:49:54 +0800

    todo app web
    for educational purposes

---
+++ todoapp/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <AddTodo/>
+    <Todos/>
+  </div>
+</template>
+
+<script>
+import AddTodo from './components/AddTodo.vue'
+import Todos from './components/Todos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
